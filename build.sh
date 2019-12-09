adb35630617cfa300fbd8cc1c95c27b1b616c02f
James Swineson <github@public.swineson.me>
2019-12-08 22:22:42 -0800

    update build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,9 @@ export GIT_COMMIT=$(git rev-list -1 HEAD)
 export CURRENT_TIME=$(date -u "+%Y-%m-%d %T UTC")
 
 mkdir -p build
+mkdir -p /tmp/go
+export GOROOT=/tmp/go
+export GO111MODULE=on
+
+go get -d ./...
 go build -ldflags "-s -w -X main.versionGitCommitHash=$GIT_COMMIT  -X main.versionCompileTime=$CURRENT_TIME" -o build/snd
