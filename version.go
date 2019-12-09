0854442e9225f283e7d3d82f508c06850cbf536a
James Swineson <github@public.swineson.me>
2019-12-10 00:12:24 +0800

    bump version to 0.1.0

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 var versionMajor = 0
-var versionMinor = 0
-var versionRevision = 1
+var versionMinor = 1
+var versionRevision = 0
 var versionGitCommitHash string
 var versionCompileTime string
